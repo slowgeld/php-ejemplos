20e95bb03a7839090052528d03ad23374f67131d
slowgeld <1526556@senati.pe>
2024-05-15 15:54:52 -0500

    Nuevas funciones

---
+++ 05-funciones.php
@@ -0,0 +1,13 @@
+<?php
+function obtenerSaludo($paremetro) {
+    return "hola como estas ".$parametro;
+}
+
+function mostrarDatos($nombres,$apellidos,$curso,$semestre)  {
+    $mensaje = "hola soy ".$nombres." ".$apellidos." y estoy en el curso ".$curso." del ".$semestre;
+    return $mensaje;
+}
+
+echo obtenerSaludo("jesus");
+echo mostrarDatos("jesus","saldaña","backend developer web","tercer semestre");
+?>
