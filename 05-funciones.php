90128db968c7521b21fcac6ce0f132d916419935
Renzo Ruiz <renfranruizzapana@gmail.com>
2024-05-15 17:08:43 -0500

    funcion de calcular una operacion

--- 05-funciones.php
+++ 05-funciones.php
@@ -8,6 +8,18 @@ function mostrarDatos($nombres,$apellidos,$curso,$semestre)  {
     return $mensaje;
 }
 
+function calcular($operador,$numero1,$numero2)  {
+    if($operador=="+")
+    return $numero1+$numero2;
+    if($operador=="-")
+    return $numero1-$numero2;
+    if($operador=="*")
+    return $numero1*$numero2;
+    if($operador=="/")
+    return $numero1/$numero2;
+}
+
 echo obtenerSaludo("jesus");
 echo mostrarDatos("jesus","saldaña","backend developer web","tercer semestre");
+echo "El total de la suma es : ".calcular("+",10,34);
 ?>
